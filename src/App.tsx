7d5fa2fa17f06432df6f13aa0e6a420811e34f70
Junho Baik <junhobaik@gmail.com>
2021-10-14 16:20:28 +0900

    feat: router 구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import store from "@/store";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <Provider store={store.store}>App</Provider>;
+  return (
+    <Provider store={store.store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
